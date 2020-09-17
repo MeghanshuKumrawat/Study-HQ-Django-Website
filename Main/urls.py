3472b275cdba3aec79742be2473d081f1e0398e5
meghanshu <meghanshukumrawat321@gmail.com>
2020-09-17 14:12:10 +0530

    adding forgot password! feature

--- Main/urls.py
+++ Main/urls.py
@@ -30,8 +30,13 @@ urlpatterns = [
     path('register/', register, name='user-register'),
     path('profile/', profile, name='user-profile'),
     path('contact/', contact, name='user-contact'),
-
+    
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
